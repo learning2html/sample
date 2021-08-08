e69f715c9bd0c86f4f202993b30534bafb510839
learning2html <84885095+learning2html@users.noreply.github.com>
2021-08-08 12:16:48 +0700

    Add files via upload

---
+++ 3-column-preview-card-component-main/code.js
@@ -0,0 +1,11 @@
+// function change(id) {
+// 	var color = id.style.color;
+// 	console.log(color);
+// 	id.style.background = "color";
+// 	console.log(id.style.background);
+// }
+// function changeAgain(id) {
+// 	console.log(id);
+// 	id.style.background = "hsla(0, 0%, 100%, 0.75)";
+	
+// }
